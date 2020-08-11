8543316ab44bcba604bb12a0e68ad86e14e3023b
SHAMI GUPTA <shami.gupta@in.ibm.com>
2020-08-11 17:32:13 +0530

    Gmail7

--- app/myRapiv4.R
+++ app/myRapiv4.R
@@ -40,13 +40,14 @@ function(a, b){
 #* @param reference Customer num or Policy Num or Claim Num
 #* @post /sendgmailGENApps
 function(myentity, operation, reference){
+  
   basemicroserviceurl <<- "http://localhost:8000/"
 
   email_qry <- ifelse(myentity=="Claim", paste("select firstname, lastname, emailaddress from vcustomer A, vpolicy B, vclaim C where A.CUSTOMERNUMBER = B.CUSTOMERNUMBER and B.POLICYNUMBER = C.POLICYNUMBER AND C.CLAIMNUMBER = ",reference,sep=""),
                       ifelse(myentity=="Policy",paste("select firstname, lastname, emailaddress from vcustomer A, vpolicy B where A.CUSTOMERNUMBER = B.CUSTOMERNUMBER and B.POLICYNUMBER = ",reference,sep=""),
                              paste("select firstname, lastname, emailaddress from vcustomer where CUSTOMERNUMBER = ",reference,sep="")))
 
-  
+  print("*******")
   print(email_qry)
     
   res <- POST(paste(basemicroserviceurl,"getDVMzEUSDocker?",sep="")
